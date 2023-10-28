f6c66350683ebbdee8639ad2363fcb1ba24f6642
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-10-28 14:17:28 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,1 +1,18 @@
+import tkinter as tk
+import main
 
+class AutomateMontavueGoGUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Automate MontavueGo")
+
+        self.button = tk.Button(self, text="Run Automation", command=self.on_button_click)
+        self.button.pack()
+
+    def on_button_click(self):
+        main.run_automation()
+
+if __name__ == "__main__":
+    gui = AutomateMontavueGoGUI()
+    gui.mainloop()
